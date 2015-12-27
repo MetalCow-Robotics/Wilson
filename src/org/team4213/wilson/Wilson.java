9d36f404f3590013f96e610bc6184dc7a495fdd8
Thaddeus Hughes <hughes.thad@gmail.com>
2015-12-27 16:24:44 -0600

    Testy testy

--- src/org/team4213/wilson/Wilson.java
+++ src/org/team4213/wilson/Wilson.java
@@ -1,16 +1,11 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.team4213.wilson;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
+ * This is the entry point!
+ * 
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation.
